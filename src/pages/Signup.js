3b616601ce49d4ee1ab3f960d6bb073a34a984de
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-18 17:17:39 -0600

    Set up config to create nes users

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,49 +1,83 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { signup } from '../firebase/auth';
 
 function Signup() {
-  return (
-    <div className="login-container">
-      <div className="ui card login-card">
-        <div className="content">
-          <form className="ui form">
-            <div className="two fields">
-              <div className="field">
-                <label>
-                  First Name
-                  <input
-                    type="text"
-                    name="firstName"
-                    placeholder="First Name"
-                  />
-                </label>
-              </div>
-              <div className="field">
-                <label>
-                  Last Name
-                  <input type="text" name="lastName" placeholder="Last Name" />
-                </label>
-              </div>
-            </div>
-            <div className="field">
-              <label>
-                Email
-                <input type="email" name="email" placeholder="Email" />
-              </label>
-            </div>
-            <div className="field">
-              <label>
-                Password
-                <input type="password" name="password" placeholder="Password" />
-              </label>
-            </div>
-            <button className="ui primary button login" type="submit">
-              Sign Up
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+	const { register, handleSubmit, reset } = useForm();
+	const [isLoading, setLoading] = React.useState(false);
+
+	const onSubmit = async (data) => {
+		setLoading(true);
+		try {
+			await signup(data);
+			reset();
+		} catch (e) {
+			console.error(e);
+		}
+		setLoading(false);
+	};
+
+	const formClassName = `ui form ${isLoading && 'loading'}`;
+
+	return (
+		<div className='login-container'>
+			<div className='ui card login-card'>
+				<div className='content'>
+					<form className={formClassName} onSubmit={handleSubmit(onSubmit)}>
+						<div className='two fields'>
+							<div className='field'>
+								<label>
+									First Name
+									<input
+										type='text'
+										name='firstName'
+										placeholder='First Name'
+										ref={register}
+									/>
+								</label>
+							</div>
+							<div className='field'>
+								<label>
+									Last Name
+									<input
+										type='text'
+										name='lastName'
+										placeholder='Last Name'
+										ref={register}
+									/>
+								</label>
+							</div>
+						</div>
+						<div className='field'>
+							<label>
+								Email
+								<input
+									type='email'
+									name='email'
+									placeholder='Email'
+									ref={register}
+								/>
+							</label>
+						</div>
+						<div className='field'>
+							<label>
+								Password
+								<input
+									type='password'
+									name='password'
+									placeholder='Password'
+									ref={register}
+								/>
+							</label>
+						</div>
+						<button className='ui primary button login' type='submit'>
+							Sign Up
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Signup;
